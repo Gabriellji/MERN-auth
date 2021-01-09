5bb6d9ffb4b56b4c03b803cc6c90c1af22fd543e
Gabriellji <gabriellji@yahoo.com>
2021-01-10 01:05:26 +0300

    feat: add script as a helper for heroku deploy

--- server.js
+++ server.js
@@ -1,15 +1,10 @@
 const express = require("express");
 const mongoose = require("mongoose");
-// const bodyParser = require("body-parser");
 const passport = require("passport");
 const users = require("./routes/api/users");
+const path = require('path');
 const app = express();
-// // Bodyparser middleware
-// app.use(
-//   bodyParser.urlencoded({
-//     extended: false
-//   })
-// );
+
 app.use(express.json());
 // DB Config
 const db = require("./config/keys").mongoURI;
@@ -30,5 +25,15 @@ app.use(passport.initialize());
 require("./config/passport")(passport);
 // Routes
 app.use("/api/users", users);
+
+// Serve static assets if in production
+
+if (process.env.NODE_ENV === 'production') {
+  app.use(express.static('client/build'));
+
+  app.get('*', (req, res) => {
+    res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
+  });
+}
 const port = process.env.PORT || 5000;
 app.listen(port, () => console.log(`Server up and running on port ${port} !`));
